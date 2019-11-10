563173ef2e2dd7f8927b71b1edc90c80d985355e
kwayep <57526434+kwayep@users.noreply.github.com>
2019-11-10 11:57:13 +0100

    Add files via upload

---
+++ depot_retrait.py
@@ -0,0 +1,10 @@
+    def depot(self, montant):
+        """ ajout d'une somme à l'attribut solde """
+
+        self.solde += montant
+def retrait(self,montant):
+    self.solde-=montant
+
+def affiche(self):
+        """ L'affichage des informations d'un compte"""
+    print("le solde du compte bancaire de %s est de %.2f FRCS" % (self.nom, selft.solde))
